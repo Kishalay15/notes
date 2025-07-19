d38062aeefa77c6a5665d5fe0b7067a9c163268f
Kishalay15 <kishalylahiri@gmail.com>
2025-07-19 23:07:55 +0530

    autofocus to fix mobile keyboard issue

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -32,6 +32,7 @@ export default function Editor({ note, onChange }: EditorProps) {
         onChange={handleContentChange}
         placeholder="Start writing your note..."
         spellCheck="false"
+        autoFocus
         style={{
           fontFamily:
             '"JetBrains Mono", "SF Mono", Monaco, "Cascadia Code", "Roboto Mono", Consolas, "Courier New", monospace',
